722a46303f01bd172ba8c9c46b4643f1cfcdb54e
gaokingK <49517991+gaokingK@users.noreply.github.com>
2022-11-30 18:08:21 +0800

    some modify

--- python/script/learn_python_property.py
+++ python/script/learn_python_property.py
@@ -1,30 +1,39 @@
 class TestProperty(object):
     def __init__(self):
-        self.__size__ = ''
+        self.__owningDevice__ = ''
 
     # @property
-    # def size(self):
-    #     return self._size
+    # def owningDevice(self):
+    #     return self._owningDevice # 带不带 __
     #
-    # @size.setter
-    # def size(self, value):
-    #     self._size = value
+    # @owningDevice.setter
+    # def owningDevice(self, device):
+    #     self._owningDevice = device
 
     @property
-    def size(self):
-        return self.__size
+    def owningDevice(self):
+        return self.__owningDevice
 
-    @size.setter
-    def size(self, value):
-        self.__size = value
+    @owningDevice.setter
+    def owningDevice(self, device):
+        # device.func() 这样就可以双向注册了
+        self.__owningDevice = device
 
     # @property
-    # def size(self):
-    #     return self.__size__
+    # def owningDevice(self):
+    #     return self.__owningDevice__
     #
-    # @size.setter
-    # def size(self, value):
-    #     self.__size__ = value
+    # @owningDevice.setter
+    # def owningDevice(self, device):
+    #     self.__owningDevice__ = device
+
+
+def debug_Demo1():
+    demo = TestProperty()
+    # demo.__owningDevice # 提示没有这个属性
+    print(demo.owningDevice)
+    demo.owningDevice = 2  # 这样设置
+    print(demo.owningDevice)
 
 
 if __name__ == '__main__':
